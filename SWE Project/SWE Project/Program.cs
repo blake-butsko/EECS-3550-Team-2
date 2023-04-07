13a5143b42fff9afa1c8475c030030aba20caa24
Daniel Viar <dviar9871@gmail.com>
2023-04-07 14:35:47 -0400

    New Branch + Load Engineer methods
    - Finished Edit Flight and Delete Flight methods (The parts that don't interact with customers)

--- SWE Project/SWE Project/Program.cs
+++ SWE Project/SWE Project/Program.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 using ClosedXML;
 using ClosedXML.Excel;
 using System.Collections;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.VariantTypes;
+using static ClosedXML.Excel.XLPredefinedFormat;
 
 // Class for global variables following c# standards
 public class Globals
@@ -28,7 +31,7 @@ public class Flight
 {
     Location FlightFrom;
     Location FlightTo;
-    DateTime FlightTime;
+    System.DateTime FlightTime;
     int FlightId;
     int PlaneType { get; set; }
     float Distance;
@@ -36,7 +39,7 @@ public class Flight
     Decimal Price { get; set; } // Using Decimal class made to deal with money cause floats and doubles loose precision over calculations
     //+passengers: List<Customer>
 
-    public Flight(int FlightId,Location FlightFrom, Location FlightTo, DateTime FlightTime)
+    public Flight(int FlightId,Location FlightFrom, Location FlightTo, System.DateTime FlightTime)
     {
         this.FlightId = FlightId;
         this.FlightFrom = FlightFrom;
@@ -93,7 +96,7 @@ public class LoadEngineer : Actor
 
     }
 
-    public void CreateFlight(int FlightId,Location DepartingFrom, Location ArrivingAt, DateTime DateTimeInformation)
+    public void CreateFlight(int FlightId,Location DepartingFrom, Location ArrivingAt, System.DateTime DateTimeInformation)
     {
         Flight newFlight = new(FlightId,DepartingFrom, ArrivingAt, DateTimeInformation);
         try
@@ -107,13 +110,17 @@ public class LoadEngineer : Actor
             listOfData.Add(FlightId);
             listOfData.Add(DepartingFrom.airport);
             listOfData.Add(ArrivingAt.airport);
-            listOfData.Add(DateTimeInformation.ToUniversalTime().ToShortDateString());
-          
-            table.InsertRowsBelow(1); // Put new flight data into list
-
+            listOfData.Add(DateTimeInformation.ToUniversalTime().ToString("g"));
+            if(!(table.DataRange.FirstRow().Cell(1).Value.IsBlank))
+            {
+                table.InsertRowsBelow(1); // Put new flight data into list
+            }
+           
+            var tableLastRow = table.LastRow();
             for(int i = 0; i < table.LastRow().CellCount(); i++) // Iterrate through last row of table hitting each cell
             {
-                table.LastRow().Cell(i + 1).Value = listOfData[i].ToString(); // Change value of cell to list data
+                tableLastRow.Cell(i + 1).Value = listOfData[i].ToString(); // Change value of cell to list data
+                
 
             }
             workbook.Save(); // Save changes
@@ -131,16 +138,187 @@ public class LoadEngineer : Actor
 
     public void EditFlight(int FlightId)
     {
-        // Find excel in excel file
+        string [] listOfAirports = { "Nashville", "Cleveland", "Los Angeles", "New York City", "Salt Lake City", "Miami", "Detroit", "Atlanta", "Chicago", "Las Vegas", "Washington DC" };
+        // Find flight id in excel file
+        try
+        {
+            var workbook = new XLWorkbook(Globals.databasePath); // Open database
+            var worksheet = workbook.Worksheet("ActiveFlights"); // Get Flight Manifest sheet
 
-        // bring up data
+            var table = worksheet.Tables.Table(0);
 
+            var idColumn = table.DataRange.Column(1);
+            for (int i = 1; i <= idColumn.CellCount(); i++)
+            {
+                if (String.Equals(idColumn.Cell(i).Value.GetText(), FlightId.ToString()))
+                {
+                    var flightRow = table.DataRange.Row(i); 
+                  
+                    String userEntry;
+                  
+                    do
+                    {
+                        Console.WriteLine("What field would you like to edit?");
+                        Console.WriteLine("To edit flight id, type: flight id");
+                        Console.WriteLine("To edit the place the plane is leaving from, type from");
+                        Console.WriteLine("To edit the place the plane is arriving, type: to");
+                        Console.WriteLine("To edit the date and time the plane is leaving, type: date");
+                        Console.WriteLine("To stop editing the flight, type: quit");
+
+;                       userEntry = Console.ReadLine().ToLower();
+                        userEntry = userEntry.Trim();
+
+                        if(String.Equals(userEntry, "flight id"))
+                        {
+                            Console.WriteLine("Current Value: " + flightRow.Cell(1).Value);
+                            Console.WriteLine("What would you like the new value to be?");
+
+                            string userChange = Console.ReadLine();
+                            int tryParseTest;
+                            if(!(Int32.TryParse(userChange, out tryParseTest)))
+                            {
+                                Console.WriteLine("Invalid input");
+                            }
+                            else 
+                            {
+                                flightRow.Cell(1).Value = userChange;
+                                workbook.Save();
+                            }
+                        }
+                        else if(String.Equals(userEntry, "from"))
+                        {
+                            Console.WriteLine("Current Value: " + flightRow.Cell(2).Value);
+                            Console.WriteLine("What would you like the new value to be? Enter the city the airport is in: ");
+
+                            string userChange = Console.ReadLine().ToLower();
+
+                            int tryParseTest;
+                            bool validAirport = false;
+                            for (int j = 0; j < listOfAirports.Length; j++)
+                            {
+                                if (userChange.IndexOf(listOfAirports[j].ToLower()) != -1)
+                                {
+                                    validAirport = true;
+                                }
+                            }
+                            if (!(Int32.TryParse(userChange, out tryParseTest) || validAirport))
+                            {
+                                Console.WriteLine("Invalid input");
+                            }
+                            else
+                            {
+                                flightRow.Cell(2).Value = userChange;
+
+                                workbook.Save();
+                            }
+
+                        }
+                        else if(String.Equals(userEntry,"to"))
+                        {
+                            Console.WriteLine("Current Value: " + flightRow.Cell(3).Value);
+                            Console.WriteLine("What would you like the new value to be? Enter the city the airport is in: ");
+
+                            string userChange = Console.ReadLine().ToLower();
+
+                            int tryParseTest;
+
+                            bool validAirport = false;
+                            for (int j = 0; j < listOfAirports.Length; j++) 
+                            {
+                                if (userChange.IndexOf(listOfAirports[j].ToLower()) != -1)
+                                {
+                                    validAirport = true;
+                                }
+                            }
+
+                            if (!(Int32.TryParse(userChange, out tryParseTest) || validAirport))
+                            {
+                                Console.WriteLine("Invalid input");
+                            }else
+                            {
+                                flightRow.Cell(3).Value = userChange;
+                                workbook.Save();
+                            }
+
+                        }
+                        else if(String.Equals(userEntry, "date"))
+                        {
+
+                            Console.WriteLine("Current Value: " + flightRow.Cell(4).Value);
+                            Console.WriteLine("What would you like the new value to be? Enter in the format month/day/year hour:minute AM/PM");
+
+                            string userChange = Console.ReadLine();
+                        
+                            try 
+                            {
+                                System.DateTime newTime = System.DateTime.Parse(userChange);
+
+                                flightRow.Cell(4).Value = userChange;
+                                workbook.Save();
+
+                            }
+                            catch(Exception e)
+                            {
+                                Console.WriteLine("Invalid Entry");
+                            }
+                          
+                           
+                           
+                        }
+
+                       
+                        Console.WriteLine();
+                    } while (!(String.Equals(userEntry,"quit")));
+
+                
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
 
 
 
     }
 
-    public void DeleteFlight(int FlightId) { }
+    public void DeleteFlight(int FlightId) 
+    {
+
+        // Find flight id in excel file
+        try
+        {
+            var workbook = new XLWorkbook(Globals.databasePath); // Open database
+            var worksheet = workbook.Worksheet("ActiveFlights"); // Get Flight Manifest sheet
+
+            var table = worksheet.Tables.Table(0);
+
+            var idColumn = table.DataRange.Column(1);
+            for (int i = 1; i <= idColumn.CellCount(); i++)
+            {
+                if (String.Equals(idColumn.Cell(i).Value.GetText(), FlightId.ToString()))
+                {
+                    var flightRow = table.DataRange.Row(i);
+
+                    flightRow.Delete();
+                    workbook.Save();  
+                    Console.WriteLine("Flight " + FlightId + " has been deleted.");
+
+                    // Update customer history
+
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+
+    }
 
     public void CreateAccount(string UserId, string Password)
     {
@@ -152,13 +330,7 @@ public class LoadEngineer : Actor
         throw new NotImplementedException();
     }
 
-
-
-
-
-
 }
-
 
 class Program
 {
@@ -169,10 +341,10 @@ class Program
 
         LoadEngineer alex = new("12345", "password");
 
-        DateTime dateTime = DateTime.Now;
+        System.DateTime dateTime = System.DateTime.Now;
         Location from = new("texas", "houston airport");
         Location to = new("Nebraska", "Nebraska airport");
-        alex.CreateFlight(123,from, to, dateTime);
+        alex.DeleteFlight(789);
     }
 
 
